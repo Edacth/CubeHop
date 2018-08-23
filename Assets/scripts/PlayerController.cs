017dfaed3f5173310070c072ba09b723a7a44f0e
Cade Anderson <cadeanderson99@gmail.com>
2018-08-23 16:51:31 -0700

    Started the jumping script

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,36 +1,66 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerController : MonoBehaviour
+﻿namespace Assets
 {
-    public CharacterController controller;
-    private float speed = 0.1f;
-    private float gravity = -0.1f;
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System.Collections;
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    public class PlayerController : MonoBehaviour
+    {
+        public CharacterController controller;
+        private float speed = 0.1f;
+        private float gravity = -1.5f;
+        private int jumpTime;
+        private IEnumerator coroutine;
+        public Vector3 characterMovement;
+        // Use this for initialization
+        void Start()
+        {
 
-        //Character Movement
-        Vector3 characterMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
-        //Debug.Log(characterMovement);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            //Character Movement
+            characterMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+            //Debug.Log(characterMovement);
             //Normalize the movement when no input
-        if (characterMovement != Vector3.zero)
-        {
-            characterMovement.Normalize();
+            if (characterMovement != Vector3.zero)
+            {
+                characterMovement.Normalize();
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                jumpTime = 1;
+            }
+            if (jumpTime > 0)
+            {
+                coroutine = Countdown(0.5f);
+                StartCoroutine(coroutine);
+                characterMovement.y = 3.0f;
+
+
+            }
+            applyGravity();
+            controller.Move(characterMovement * speed);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        private IEnumerator Countdown(float waitTime)
         {
-            characterMovement.y = 1.0f;
-            
-        }
-        
+            yield return new WaitForSeconds(waitTime);
+            jumpTime--;
+            Debug.Log(jumpTime);
 
+        }
+
+        void applyGravity()
+        {
+            characterMovement.y += gravity;
+        }
     }
+
+    
 }
