92290a7ea8f39286cb243ad2e60aff84e1edf3c8
Cade Anderson <cadeanderson99@gmail.com>
2018-08-24 17:34:04 -0700

    I made my own character controller. Look at me mom

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -8,19 +8,43 @@
 
     public class PlayerController : MonoBehaviour
     {
-        public CharacterController controller;
-        public Text VelText;
-        private float speed = 0.2f;
-        private float gravity = -0.1f;
-        private int jumpTime;
+        //public CharacterController controller;
+
+        //Delaring Text objects and junk
+        private Text VelTextObject;
+        private Text PosTextObject;
+        private Text HighPointTextObject;
+        public Text velText;
+        public Text posText;
+        public Text highPointText;
+        private Canvas _canvas;
+        public Canvas canvas;
+
+        //Declare the rest of them
+        float highestPoint = 0;
+        private float speed = 0.7f;
+        private float gravity = -4.5f;
         private IEnumerator coroutine;
         public Vector3 characterMovement;
+        public GameObject player;
+        public float fallMultiplier = 2.5f;
+        public float lowJumpMultiplier = 2f;
+        public Rigidbody rigidbody;
+        float distToGround = 0.5f;
+
         // Use this for initialization
         void Start()
         {
+            //Starts the coroutine that increments gravity
             coroutine = Countdown(0.1f);
-            StartCoroutine(coroutine);
-            
+            //StartCoroutine(coroutine);
+
+            //Creates the canvas for UI
+            _canvas = Instantiate<Canvas>(canvas);
+            VelTextObject = Instantiate<Text>(velText, _canvas.transform);
+            PosTextObject = Instantiate<Text>(posText, _canvas.transform);
+            HighPointTextObject = Instantiate<Text>(highPointText, _canvas.transform);
+
         }
 
         // Update is called once per frame
@@ -28,31 +52,47 @@
         {
 
             //Character Movement
-            characterMovement = new Vector3(Input.GetAxisRaw("Horizontal"), characterMovement.y, 0);
-            //Debug.Log(characterMovement);
-            //Normalize the movement when no input
-            if (characterMovement != Vector3.zero)
+            characterMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
+
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
             {
-               // characterMovement.Normalize();
+                //characterMovement.y = 0.25f;
+                rigidbody.velocity = new Vector3 (rigidbody.velocity.x, 22.25f, rigidbody.velocity.z);
             }
 
-            if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
+            if (Input.GetKeyDown(KeyCode.F) && isGrounded())
             {
-                //jumpTime = 5;
-
-                characterMovement.y = 1.0f;
+                highestPoint = 0.0f;
             }
 
-            /*
-            if (jumpTime > 0)
+            if (player.transform.position.y > highestPoint)
             {
-                characterMovement.y = 1.5f;
+                highestPoint = player.transform.position.y;
             }
-            applyGravity();
-            */
-            VelText.text = (characterMovement.ToString());
-            controller.Move(characterMovement * speed);
+
+            if (characterMovement.y > -1.0f)
+            {
+                characterMovement.y += gravity * Time.deltaTime;
+            }
+
             
+
+
+            if (rigidbody.velocity.y < 0)
+            {
+                characterMovement += Vector3.up * gravity * (fallMultiplier - 1) * Time.deltaTime;
+                
+            }
+            rigidbody.velocity = new Vector3 (0, rigidbody.velocity.y, 0);
+            characterMovement.x = characterMovement.x * speed;
+            characterMovement.z = characterMovement.z * speed;
+            rigidbody.AddForce(characterMovement, ForceMode.Impulse);
+
+            //Update the UI text
+            VelTextObject.text = ("Velocity " + characterMovement.ToString());
+            PosTextObject.text = ("Position " + player.transform.position.ToString());
+            HighPointTextObject.text = ("Time " + Time.deltaTime);
+
         }
 
         private IEnumerator Countdown(float waitTime)
@@ -61,32 +101,28 @@
             {
                 yield return new WaitForSeconds(waitTime);
 
-                if (controller.isGrounded == false)
+                if (isGrounded() == false)
                 {
-                    characterMovement.y += gravity;
+                    applyGravity();
                     Debug.Log(characterMovement.y);
                 }
-                
-                
-                
-               
-                
-                /*
-               if (jumpTime > 0)
-               {
-                   jumpTime--;
-                   Debug.Log(jumpTime);
-               }
-               */
             }
 
         }
 
         void applyGravity()
         {
+            if (characterMovement.y > -1.0f)
+            {
+                characterMovement.y += gravity;
+            }
+            
+            
+        }
 
-            characterMovement.y += gravity;
-            Debug.Log(jumpTime);
+        bool isGrounded()
+        {
+            return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
         }
     }
 
