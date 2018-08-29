11e9a46768e6459985457ff3b065d483050af7cb
Edacth <cadeanderson99@gmail.com>
2018-08-28 21:14:49 -0700

    Stopped the spidey action

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -35,10 +35,6 @@
         // Use this for initialization
         void Start()
         {
-            //Starts the coroutine that increments gravity
-            coroutine = Countdown(0.1f);
-            //StartCoroutine(coroutine);
-
             //Creates the canvas for UI
             _canvas = Instantiate<Canvas>(canvas);
             VelTextObject = Instantiate<Text>(velText, _canvas.transform);
@@ -54,10 +50,19 @@
             //Character Movement
             characterMovement = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
 
+            if (isLeftWalled() && characterMovement.x < 0)
+            {
+                characterMovement.x = 0;
+            }
+            if (isRightWalled() && characterMovement.x > 0)
+            {
+                characterMovement.x = 0;
+            }
+
             if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
             {
                 //characterMovement.y = 0.25f;
-                rigidbody.velocity = new Vector3 (rigidbody.velocity.x, 22.25f, rigidbody.velocity.z);
+                rigidbody.velocity = new Vector3(rigidbody.velocity.x, 22.25f, rigidbody.velocity.z);
             }
 
             if (Input.GetKeyDown(KeyCode.F) && isGrounded())
@@ -75,15 +80,16 @@
                 characterMovement.y += gravity * Time.deltaTime;
             }
 
-            
+            Debug.DrawRay(new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z), new Vector3(0, -2.5f, 0), Color.blue);
+            Debug.DrawRay(new Vector3(transform.position.x + -0.5f, transform.position.y, transform.position.z), new Vector3(0, -2.5f, 0), Color.blue);
 
 
             if (rigidbody.velocity.y < 0)
             {
                 characterMovement += Vector3.up * gravity * (fallMultiplier - 1) * Time.deltaTime;
-                
+
             }
-            rigidbody.velocity = new Vector3 (0, rigidbody.velocity.y, 0);
+            rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, 0);
             characterMovement.x = characterMovement.x * speed;
             characterMovement.z = characterMovement.z * speed;
             rigidbody.AddForce(characterMovement, ForceMode.Impulse);
@@ -91,22 +97,7 @@
             //Update the UI text
             VelTextObject.text = ("Velocity " + characterMovement.ToString());
             PosTextObject.text = ("Position " + player.transform.position.ToString());
-            HighPointTextObject.text = ("Time " + Time.deltaTime);
-
-        }
-
-        private IEnumerator Countdown(float waitTime)
-        {
-            while (true)
-            {
-                yield return new WaitForSeconds(waitTime);
-
-                if (isGrounded() == false)
-                {
-                    applyGravity();
-                    Debug.Log(characterMovement.y);
-                }
-            }
+            HighPointTextObject.text = ("" + isLeftWalled());
 
         }
 
@@ -116,13 +107,41 @@
             {
                 characterMovement.y += gravity;
             }
-            
-            
+
+
         }
 
         bool isGrounded()
         {
-            return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+            bool collision = false;
+            if ((Physics.Raycast(new Vector3(transform.position.x + 0.49f, transform.position.y, transform.position.z), -Vector3.up, distToGround + 0.1f)) || (Physics.Raycast(new Vector3(transform.position.x - 0.49f, transform.position.y, transform.position.z), -Vector3.up, distToGround + 0.1f)))
+            {
+
+                collision = true;
+            }
+            return collision;
+        }
+
+        bool isLeftWalled()
+        {
+            bool collision = false;
+            if ((Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 0.49f, transform.position.z), -Vector3.right, distToGround + 0.1f)) || (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + -0.49f, transform.position.z), -Vector3.right, distToGround + 0.1f)))
+            {
+
+                collision = true;
+            }
+            return collision;
+        }
+
+        bool isRightWalled()
+        {
+            bool collision = false;
+            if ((Physics.Raycast(new Vector3(transform.position.x, transform.position.y + 0.49f, transform.position.z), -Vector3.left, distToGround + 0.1f)) || (Physics.Raycast(new Vector3(transform.position.x, transform.position.y + -0.49f, transform.position.z), -Vector3.left, distToGround + 0.1f)))
+            {
+
+                collision = true;
+            }
+            return collision;
         }
     }
 
